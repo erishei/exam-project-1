603411204e8bed46e5221fa327ea67a59c608e4f
Erica Sheidai <erica.sheidai@gmail.com>
2024-05-30 15:06:43 +0200

    chore: a whole lot of css

--- js/post.js
+++ js/post.js
@@ -24,7 +24,6 @@ async function fetchPost(postId) {
             throw new Error('Failed to fetch post.');
         }
         const postData = await response.json();
-        // console.log('Post Data:', postData);
         return postData;
     } catch (error) {
         console.error('Error fetching post:', error);
@@ -36,12 +35,18 @@ function displayPost(post) {
     const postData = post.data;
 
     const postContainer = document.getElementById('post-container');
+    const slicedData = (postData.created).slice(0,10)
     if (postData && postData.author && postData.author.name) {
         postContainer.innerHTML = `
             <h2 class="single-post-title">${postData.title}</h2>
+            <div class="desktop-outer-div">
             <img src="${postData.media.url}" alt="${postData.media.alt}" class="single-post-img"> 
+            <section class="desktop-section">
             <p class="single-post-author">Author: ${postData.author.name}</p>
+            <p class="single-post-author">Created: ${slicedData}</p>
             <p class="single-post-p">${postData.body}</p>
+            </section>
+            </div>
         `;
     } else {
         postContainer.innerHTML = `
